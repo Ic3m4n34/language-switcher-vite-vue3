2c036524edaf8ac47a7825ef5a2467086d6adf0f
Nico Meyer <nicomeyer@hey.com>
2021-08-25 08:44:05 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Hello
+  </div>
+</template>
+
+<script setup>
+const activeLocale = window.location.pathname.split('/')[1];
+console.log('hello', activeLocale)
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
